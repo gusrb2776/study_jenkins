892f1668e9f2ddbc94e15c5d586f5af46f337da2
이현규 <gusrb2776@naver.com>
2024-09-08 16:20:33 +0900

    안녕

---
+++ src/main/java/study/study_jenkins/StudyJenkinsApplication.java
@@ -0,0 +1,13 @@
+package study.study_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJenkinsApplication.class, args);
+	}
+
+}
